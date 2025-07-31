b098fc7f6c4924e79be190c0b5958134300db7a7
seoljeonga <seoljeonga@gmail.com>
2025-07-31 16:07:33 +0900

    message에 v1추가

--- routers/memberRouter.js
+++ routers/memberRouter.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const { User } = require('../models/index');
+const express = require("express");
+const { User } = require("../models/index");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { RequestPolicyOptions } = require('@azure/storage-blob');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { RequestPolicyOptions } = require("@azure/storage-blob");
 const secret = process.env.JWT_SECRET;
 
 const createHash = async (password, saltRound) => {
@@ -12,7 +12,7 @@ const createHash = async (password, saltRound) => {
   return hashed;
 };
 
-router.post('/sign-up', async (req, res, next) => {
+router.post("/sign-up", async (req, res, next) => {
   const member = req.body;
   const newPassword = await createHash(member.password, 10);
   member.password = newPassword;
@@ -22,17 +22,17 @@ router.post('/sign-up', async (req, res, next) => {
     res.status(201).json({
       success: true,
       member: result,
-      message: '회원가입이 완료되었습니다.',
+      message: "회원가입이 완료되었습니다.v",
     });
   } catch (err) {
     next(err, req, res);
   }
 });
 
-router.post('/sign-in', async (req, res, next) => {
+router.post("/sign-in", async (req, res, next) => {
   const { userName, password } = req.body;
   const options = {
-    attributes: ['password', 'userName', 'id'],
+    attributes: ["password", "userName", "id"],
     where: { userName: userName },
   };
   try {
@@ -40,7 +40,7 @@ router.post('/sign-in', async (req, res, next) => {
     if (result) {
       const compared = await bcrypt.compare(password, result.password);
       if (compared) {
-        const token = jwt.sign({ uid: result.id, rol: 'admin' }, secret);
+        const token = jwt.sign({ uid: result.id, rol: "admin" }, secret);
         res.status(200).json({
           success: true,
           token: token,
@@ -48,16 +48,16 @@ router.post('/sign-in', async (req, res, next) => {
             userName,
             userName: result.userName,
           },
-          message: '로그인에 성공했습니다.',
+          message: "로그인에 성공했습니다.",
         });
       } else {
         res.status(401).json({
-          message: '비밀번호가 잘못되었습니다.',
+          message: "비밀번호가 잘못되었습니다.",
         });
       }
     } else {
       res.status(404).json({
-        message: '존재하지않는 아이디입니다.',
+        message: "존재하지않는 아이디입니다.",
       });
     }
   } catch (err) {
@@ -65,7 +65,7 @@ router.post('/sign-in', async (req, res, next) => {
   }
 });
 
-router.post('/regist-apns', async (req, res, next) => {
+router.post("/regist-apns", async (req, res, next) => {
   console.log(req.body);
   const { userName, deviceToken } = req.body;
   try {
@@ -79,11 +79,11 @@ router.post('/regist-apns', async (req, res, next) => {
         member: {
           userName: userName,
         },
-        message: 'device token 등록에 성공했습니다.',
+        message: "device token 등록에 성공했습니다.",
       });
     } else {
       res.status(404).json({
-        message: '존재하지않는 아이디입니다.',
+        message: "존재하지않는 아이디입니다.",
       });
     }
   } catch (err) {
